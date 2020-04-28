fdc094ecf04ba6614ccbde8f786c481fcd6f3fe2
kmuhin <playmyx@mail.ru>
2020-04-29 02:06:54 +0300

    v0.4

--- test_sqlite3.py
+++ test_sqlite3.py
@@ -3,28 +3,50 @@ from pprint import pprint
 import sqlite3
 
 
-def s():
+def printdata():
+    print(' number of regions: ', end='')
+    sqlstring = 'select count(1) from regions'
+    cursor.execute(sqlstring)
+    print(cursor.fetchall()[0][0])
+
     cursor.execute('select * from regions')
     print('from regions:')
+    pprint([i[0] for i in cursor.description])
     pprint(cursor.fetchall())
+
+    print(' number of records cases: ', end='')
+    sqlstring = 'select count(1) from CasesADAY'
+    cursor.execute(sqlstring)
+    print(cursor.fetchall()[0][0])
+
     cursor.execute(
-        'select datetime(date_int,"unixepoch"),* from CasesADAY LEFT JOIN regions ON CasesADAY.region_id=regions.id')
+        'select datetime(date_int,"unixepoch"),* from CasesADAY'
+        ' LEFT JOIN regions ON CasesADAY.region_id=regions.id LIMIT 100')
     # cursor.execute('select datetime(date_int,"unixepoch"),* from CasesADAY')
     print('from CasesADAY:')
     pprint(cursor.fetchall())
 
+    print(' number of cases with incorrect region: ', end='')
+    sqlstring = 'SELECT count(1) FROM CasesADAY' \
+                ' WHERE NOT EXISTS' \
+                ' ( SELECT 1 FROM regions ' \
+                '   WHERE regions.id = CasesADAY.region_id )'
+    cursor.execute(sqlstring)
+    print(cursor.fetchall()[0][0])
+
+
 def dbinsertregion(conn, data):
     cursor = conn.cursor()
-    cursor.execute('insert or ignore into regions(region,regionISO) values(?,?)', data)
+    cursor.execute('insert or replace into regions(region,regionISO) values(?,?)', data)
     region_id = cursor.lastrowid
     cursor.close()
-    return  region_id
+    return region_id
 
 
 def dbinsert(conn, data):
     cursor = conn.cursor()
-    cursor.execute('insert or replace into CasesADay(date_int,region_id,cases,deaths) values(?,?,15,0)',
-                   [data[0].replace(tzinfo=timezone.utc).timestamp(), data[1]])
+    data[0] = data[0].replace(tzinfo=timezone.utc).timestamp()
+    cursor.execute('insert or replace into CasesADay(date_int,region_id, cases,deaths) values(?,?,?,?)', data)
     cursor.close()
     return cursor.lastrowid
 
@@ -54,24 +76,20 @@ pprint(cursor.fetchall())
 cursor.execute('select * from regions')
 data = cursor.fetchall()
 dataiso = {i[2]: i[0:2] for i in data}
-today = datetime.combine(date.today(),time(4,0))
-
-if 'RUS' in dataiso.keys():
-    region_id = dataiso['RUS'][0]
-else:
-    region_id = dbinsertregion(conn,['RUSSIA', 'RUS'])
+today = datetime.combine(date.today(), time(4, 0))
 
 
-dbinsert(conn, [today, region_id])
+def addcase(region, regionISO, cases, deaths):
+    if regionISO in dataiso.keys():
+        region_id = dataiso[regionISO][0]
+    else:
+        region_id = dbinsertregion(conn, [region, regionISO])
+    dbinsert(conn, [today, region_id, cases, deaths])
 
-if 'USA' in dataiso.keys():
-    region_id = dataiso['USA'][0]
-else:
-    region_id = dbinsertregion(conn, ['USA', 'USA'])
 
+addcase('RUSSIA', 'RUS', 100, 0)
+addcase('US', 'USA', 200, 0)
 
-dbinsert(conn, [today, region_id])
-
-s()
+printdata()
 conn.commit()
 conn.close()
