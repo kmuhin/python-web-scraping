105d2e1016d9672731e4f127f20529b07fdd6d83
kmuhin <playmyx@mail.ru>
2020-06-12 18:26:56 +0300

    v1.3 added checking existing picture added field url to data added returning data with schema added config file added filter keys of data

--- beltextil/cleartext.py
+++ beltextil/cleartext.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import configparser
 from pathlib import Path
 import requests
 import urllib
@@ -11,26 +12,39 @@ __version__ = '1.0'
 # string replaces
 
 
-url = 'https://www.beltextil.ru/catalog/1100-kpb-15-sp-valeri'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
 }
-#pathpictures = 'C:/Users/user/Desktop/Рабочее/'
+
+workdir = Path(__file__).parent.absolute()
+workfile = Path(__file__).absolute()
+
+config = configparser.ConfigParser()
+configname = str(workdir) + '\\' + workfile.stem + '.ini'
+config.read(configname)
+DataKeysForPrice = ['Артикул']
+
 pathpictures = ''
+try:
+    pathpictures = config['DEFAULT']['pathpictures'].strip("'")
+except KeyError:
+    pass
+
 
 def remove_characters(value, deletechars):
     for c in deletechars:
-        value = value.replace(c,'')
+        value = value.replace(c, '')
     return value;
 
-def download_file_rewrite(url, filename='', workdir='', rewrite=False):
-    if workdir:
-        workdir += '/'
-    else:
-        workdir = str(Path().absolute())
+
+def download_file_rewrite(url, filename='', path='', rewrite=False):
+    if path and not path.endswith('\\'):
+        path += '\\'
+    if not Path(path).is_dir():
+        path = ''
     if not filename:
         filename = url.split('/')[-1]
-    local_filename = workdir + filename
+    local_filename = path + filename
     if Path(local_filename).is_file() and not rewrite:
         return local_filename
     # NOTE the stream=True parameter below
@@ -43,6 +57,7 @@ def download_file_rewrite(url, filename='', workdir='', rewrite=False):
                     # f.flush()
     return local_filename
 
+
 def getinfofromurl(url):
     #  make schema dictionary
     data = dict.fromkeys(('attrs', 'price'))
@@ -54,16 +69,16 @@ def getinfofromurl(url):
     data['price'] = {}
     # debug save html
     with open(f'tmp.html', 'wb') as f:
-            f.write(response.content)
+        f.write(response.content)
     #
     soup = BeautifulSoup(response.text, 'html.parser')
-    filename = remove_characters(soup.html.head.title.text, '\/:*?"<>|')+'.jpg'
+    filename = remove_characters(soup.html.head.title.text, '\/:*?"<>|') + '.jpg'
     # html - body - div.document - div.main - div.product-view
     # html - body - div.document - div.main - div.product-view - div.pictures - div.front-image - a
     picture = soup.find('div', attrs={'class': 'front-image'})
-    picture_url =  picture.a.attrs['href']
-    #urllib.request.urlretrieve(picture_url, pathpictures+filename)
-    filename = download_file_rewrite(picture_url, filename=filename, workdir=pathpictures)
+    picture_url = picture.a.attrs['href']
+    # urllib.request.urlretrieve(picture_url, pathpictures+filename)
+    filename = download_file_rewrite(picture_url, filename=filename, path=pathpictures)
     # html - body - div.document - div.main - div.product-view - div.info
     info = soup.find('div', attrs={'class': 'info'})
     attributes = info.find('div', attrs={'class': 'attributes'})
@@ -71,7 +86,10 @@ def getinfofromurl(url):
     # html - body - div.document - div.main - div.product-view - div.attributes
     for child in attributes:
         if child.name:
-            data['attrs'][child.dt.text] = child.dd.text
+            if child.dt.text in DataKeysForPrice:
+                data['price'][child.dt.text] = child.dd.text
+            else:
+                data['attrs'][child.dt.text] = child.dd.text
 
     # html - body - div.document - div.main - div.product-view - div.info - div.price-helper
     pricehelper = info.find('div', attrs={'class': 'price-helper'})
@@ -81,15 +99,17 @@ def getinfofromurl(url):
     # чищу мусор из строк. получаю чистые числа
     number = number.replace('В наличии: ', '').replace(' шт.', '')
     price = price.replace('Цена: ', '').replace(' р.', '').replace(' ', '')
-    data['price']={'Цена': price, 'Количество': number, 'picture' : filename, 'url': url}
+    data['price'] = {**data['price'], 'Цена': price, 'Количество': number, 'Картинка': filename, 'Ссылка': url}
     return data
+
 
 def info(url):
     data = getinfofromurl(url)
     for i in data:
         print(f'{i:20} {data[i]}')
 
-def main():
+
+def main() -> object:
     info('https://www.beltextil.ru/catalog/14s80-shr-v-up-215148-kpb-kvartet-ris-7-cv-12-korall')
     while True:
         try:
@@ -100,6 +120,10 @@ def main():
             print()
         except requests.exceptions.MissingSchema:
             print('Invalid URL. Try again.')
+
+
+if __name__ == '__main__':
+    main()
 
 if __name__ == '__main__':
     main()
